dc45134ab2ea9eb4bad74238d2ad5c53029b91d5
Souvik <souvikghosh957@gmail.com>
2021-12-15 20:22:38 +0530

    fix issues

--- src/main/java/com/parking/booking/repository/DateTimeRepository.java
+++ src/main/java/com/parking/booking/repository/DateTimeRepository.java
@@ -17,9 +17,8 @@ public interface DateTimeRepository extends JpaRepository<DateTime, String>{
 //			+ "and location_id = :locationId", nativeQuery=true)
 //	public List<DateTime> getTimeSlotIfAvailable(Date timeFrom, Date timeTo, String locationId);
 	
-	@Query(value="select * from date_time dt where dt.entry_time BETWEEN :timeFrom "
-			+ "and :timeTo or dt.exit_time BETWEEN :timeFrom and :timeTo "
-			+ "and location_id = :locationId", nativeQuery=true)
+	@Query(value="select * from date_time dt where location_id = :locationId and dt.entry_time BETWEEN :timeFrom "
+			+ "and :timeTo or dt.exit_time BETWEEN :timeFrom and :timeTo", nativeQuery=true)
 	public List<DateTime> getTimeSlotIfAvailable(Date timeFrom, Date timeTo, String locationId);
 	
 
